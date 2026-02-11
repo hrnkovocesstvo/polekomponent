297fa73025762a97ccdca14626b51d07ca973020
BabnicA.itb25@vsps-su.cz <BabnicA.itb25@vsps-su.cz>
2026-02-11 09:32:13 +0100

    neco jsem udelal

--- polekomponent/Form1.cs
+++ polekomponent/Form1.cs
@@ -16,5 +16,24 @@ namespace polekomponent
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int left = 30;
+            Button[] buttonArray = new Button[100];
+            for (int i = 0; i < buttonArray.Length; i++)
+            {
+                buttonArray[i] = new Button();
+                buttonArray[i].Size = new Size(50, 23); buttonArray[i].Location = new Point(left, 50);
+                buttonArray[i].Text = Convert.ToString(i);
+                buttonArray[i].Click += new System.EventHandler(this.button_Click); this.Controls.Add(buttonArray[i]);
+                left += 56;
+            }
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Text = "Klik!";
+        }
     }
 }
